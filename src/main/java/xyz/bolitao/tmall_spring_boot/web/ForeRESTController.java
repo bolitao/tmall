6b82836c7a88d4c3cfa0c6ee1fbaa54b82aee2a8
Boli Tao <tblsyx@outlook.com>
2019-11-13 11:42:17 +0800

    完成基础搜索功能

--- src/main/java/xyz/bolitao/tmall_spring_boot/web/ForeRESTController.java
+++ src/main/java/xyz/bolitao/tmall_spring_boot/web/ForeRESTController.java
@@ -137,4 +137,15 @@ public class ForeRESTController {
         }
         return category;
     }
+
+    @PostMapping("foresearch")
+    public Object search(String keyword) {
+        if (null == keyword) {
+            keyword = "";
+        }
+        List<Product> ps = productService.search(keyword, 0, 20);
+        productImageService.setFirstProductImages(ps);
+        productService.setSaleAndReviewNumber(ps);
+        return ps;
+    }
 }
